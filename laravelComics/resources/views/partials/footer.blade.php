38d5cf76e75f645957b8c663e5b86277971f6b87
FabrizioRozzi <rozzi458@gmail.com>
2021-06-22 15:48:12 +0200

    manca main e collegamenti menu scheletro fatto

---
+++ laravelComics/resources/views/partials/footer.blade.php
@@ -0,0 +1,131 @@
+<div class="footer-top">
+  <div class="container-80">
+    <div class="container-50">
+    <ul>
+      <li>
+        <h3>dc comics</h3>
+      </li>
+      <li>
+        <a href="#">Characters</a>
+      </li>
+      <li>
+        <a href="#">Comics</a>
+      </li>
+      <li>
+        <a href="#">Movies</a>
+      </li>
+      <li>
+        <a href="#">TV</a>
+      </li>
+      <li>
+        <a href="#">Games</a>
+      </li>
+      <li>
+        <a href="#">Videos</a>
+      </li>
+      <li>
+        <a href="#">News</a>
+      </li>
+      <li>
+        <h3>shop</h3>
+      </li>
+      <li>
+        <a href="#">Shop DC</a>
+      </li>
+      <li>
+        <a href="#">Shop DC Collectibles</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <h3>dc</h3>
+      </li>
+      <li>
+        <a href="#">Terms Of Use</a>
+      </li>
+      <li>
+        <a href="#">Privacy Policy (New)</a>
+      </li>
+      <li>
+        <a href="#">Add Choices</a>
+      </li>
+      <li>
+        <a href="#">Advertising</a>
+      </li>
+      <li>
+        <a href="#">Jobs</a>
+      </li>
+      <li>
+        <a href="#">Subscriptions</a>
+      </li>
+      <li>
+        <a href="#">Talent Workshops</a>
+      </li>
+      <li>
+        <a href="#">CPSC Certificates</a>
+      </li>
+      <li>
+        <a href="#">Ratings</a>
+      </li>
+      <li>
+        <a href="#">Shop Help</a>
+      </li>
+      <li>
+        <a href="#">Contact Us</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <h3>sites</h3>
+      </li>
+      <li>
+        <a href="#">DC</a>
+      </li>
+      <li>
+        <a href="#">MAD Magazine</a>
+      </li>
+      <li>
+        <a href="#">DC Kids</a>
+      </li>
+      <li>
+        <a href="#">DC Universe</a>
+      </li>
+      <li>
+        <a href="#">DC Power Visa</a>
+      </li>
+    </ul>
+    </div>
+    <div class="logo">
+      
+    </div>
+  </div>
+</div>
+<div class="footer-bottom">
+  <div class="container-80">
+    <div class="left">
+      <button><h3>SIGN-UP NOW!</h3></button>
+    </div>
+    <div class="right">
+      <ul>
+        <li>
+          <h3>FOLLOW US</h3>
+        </li>
+        <li>
+          <img src="../images/footer-facebook.png" alt="Facebook">
+        </li>
+        <li>
+          <img src="../images/footer-twitter.png" alt="Facebook">
+        </li>
+        <li>
+          <img src="../images/footer-youtube.png" alt="Facebook">
+        </li>
+        <li>
+          <img src="../images/footer-pinterest.png" alt="Facebook">
+        </li>
+        <li>
+          <img src="../images/footer-periscope.png" alt="Facebook">
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
---
+++ laravelComics/resources/views/partials/header.blade.php
@@ -0,0 +1,36 @@
+<div class="container-80">
+  <img src={{asset('images/dc-logo.png')}} alt="logo">
+  
+  <nav>
+    <ul>
+      <li>
+        <a href="#">CHARACTERS</a>
+      </li>
+      <li>
+        <a href="#">COMICS</a>
+      </li>
+      <li>
+        <a href="#">TV</a>
+      </li>
+      <li>
+        <a href="#">GAMES</a>
+      </li>
+      <li>
+        <a href="#">COLLECTIBLES</a>
+      </li>
+      <li>
+        <a href="#">VIDEOS</a>
+      </li>
+      <li>
+        <a href="#">FANS</a>
+      </li>
+      <li>
+        <a href="#">NEWS</a>
+      </li>
+      <li>
+        <a href="#">SHOP</a>
+      </li>
+    </ul>
+  </nav>
+  
+</div>
